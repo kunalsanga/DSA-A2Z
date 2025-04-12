782b04f56ea268d4d9344a9d673512d0b9c868be
kunalsanga <kunalsanga197@gmail.com>
2025-04-12 19:57:54 +0530

    add input code

---
+++ ip1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter the value of x and y: ";
+    cin >>x>>y;
+    cout<<"the value of x is "<<x<<" and y is "<<y;
+    return 0;
+}
