e77ec541cca60e631d8ed9b61fbf43590fcb8452
kunalsanga <kunalsanga197@gmail.com>
2025-09-01 20:34:57 +0530

    more q

---
+++ display_you_name.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+int main(){
+    std::string name;
+    std::cout<<"enter you name: ";
+    std::getline(std::cin,name); //reads all lines including whitespace
+    std::cout<<name;
+    return 0;
+}
---
+++ rectangle_area.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length;
+    int breadth;
+    int area;
+    cout<<"enter the length: ";
+    cin>>length;
+    cout<<"enter the breadth: ";
+    cin>>breadth;
+    area = length * breadth;
+    cout<<"the area of rectangle is "<<area;
+    return 0;
+}
